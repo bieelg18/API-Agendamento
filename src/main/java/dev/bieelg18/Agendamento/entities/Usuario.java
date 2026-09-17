f7fcda20f1b0e70530c21b3d7a514791219ae9c3
bieelg18 <gabriel_mjj.alves@hotmail.com>
2026-09-17 01:15:33 -0300

    feat: Implementa o Spring Security

--- src/main/java/dev/bieelg18/Agendamento/entities/Usuario.java
+++ src/main/java/dev/bieelg18/Agendamento/entities/Usuario.java
@@ -6,7 +6,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -15,7 +19,7 @@ import java.util.List;
 @Getter
 @Setter
 @Table(name = "tb_usuarios")
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,5 +40,43 @@ public class Usuario {
 
     @OneToMany(mappedBy = "cliente")
     private List<Agendamento> agendamento;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return List.of(
+                new SimpleGrantedAuthority("ROLE_" + permissao.name())
+        );
+    }
+
+    @Override
+    public String getPassword(){
+        return senha;
+    }
+
+    @Override
+    public String getUsername(){
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled(){
+        return true;
+    }
 
 }
---
+++ src/main/java/dev/bieelg18/Agendamento/security/UsuarioDetailsService.java
@@ -0,0 +1,25 @@
+package dev.bieelg18.Agendamento.security;
+
+import dev.bieelg18.Agendamento.repositories.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioDetailsService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+        throws UsernameNotFoundException{
+        return usuarioRepository.findByEmail(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("Usuário não encontrado"));
+    }
+
+}
