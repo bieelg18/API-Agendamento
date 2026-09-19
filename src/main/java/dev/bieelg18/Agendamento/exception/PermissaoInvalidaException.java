7bb4bbaec61343f9da273032fc72e8309a3d5131
bieelg18 <gabriel_mjj.alves@hotmail.com>
2026-09-18 23:21:29 -0300

    feat: Cria uma nova exception para tratar casos de permissão inválida

---
+++ src/main/java/dev/bieelg18/Agendamento/exception/PermissaoInvalidaException.java
@@ -0,0 +1,9 @@
+package dev.bieelg18.Agendamento.exception;
+
+public class PermissaoInvalidaException extends RuntimeException{
+
+    public PermissaoInvalidaException(String mensagem){
+        super(mensagem);
+    }
+
+}
