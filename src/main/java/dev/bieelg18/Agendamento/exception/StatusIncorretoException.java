512fa573caec0de06c66be9e4121f8f6aaed0282
bieelg18 <gabriel_mjj.alves@hotmail.com>
2026-09-18 22:54:04 -0300

    feat: Cria um nova exception para tratar casos de status incorreto nos agendamentos

---
+++ src/main/java/dev/bieelg18/Agendamento/exception/StatusIncorretoException.java
@@ -0,0 +1,9 @@
+package dev.bieelg18.Agendamento.exception;
+
+public class StatusIncorretoException extends RuntimeException{
+
+    public StatusIncorretoException(String mensagem){
+        super(mensagem);
+    }
+
+}
