e22c2e6b13ca8b75758002e9cacfefa6152a6f21
bieelg18 <gabriel_mjj.alves@hotmail.com>
2026-09-19 00:21:43 -0300

    feat: Cria um método de listar usuários pela permissão no repository

--- src/main/java/dev/bieelg18/Agendamento/repositories/UsuarioRepository.java
+++ src/main/java/dev/bieelg18/Agendamento/repositories/UsuarioRepository.java
@@ -1,12 +1,16 @@
 package dev.bieelg18.Agendamento.repositories;
 
 import dev.bieelg18.Agendamento.entities.Usuario;
+import dev.bieelg18.Agendamento.enums.Permissao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
     Optional<Usuario> findByEmail(String email);
+
+    List<Usuario> findByPermissao(Permissao permissao);
 
 }
